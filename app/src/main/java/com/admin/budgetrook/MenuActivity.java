d6c5e5fd332dd1b0fb986aa62ff1174ce94f0037
oskar194 <worminion@gmail.com>
2019-02-01 01:25:41 +0100

    Added mock toolbar and drawer to main menu

--- app/src/main/java/com/admin/budgetrook/MenuActivity.java
+++ app/src/main/java/com/admin/budgetrook/MenuActivity.java
@@ -5,7 +5,14 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Debug;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -27,7 +34,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MenuActivity extends Activity {
+public class MenuActivity extends AppCompatActivity {
 
     private static final String TAG = "budgetrook";
 
@@ -36,6 +43,9 @@ public class MenuActivity extends Activity {
     private String msg = "BudgetRookApp: ";
     private TextView notificationMessage;
     private LinearLayout notificationLayout;
+
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onResume() {
@@ -50,6 +60,56 @@ public class MenuActivity extends Activity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: HI");
         setContentView(R.layout.activity_menu);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
         chart = (PieChart) findViewById(R.id.chart);
         notificationMessage = (TextView) findViewById(R.id.notification_tv);
         notificationLayout = (LinearLayout) findViewById(R.id.notification_layout);
@@ -140,6 +200,16 @@ public class MenuActivity extends Activity {
             notificationMessage.setText("You currently have: " + expensesToReview + " expenses to review");
             notificationLayout.setVisibility(View.VISIBLE);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
